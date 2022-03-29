78b62a44ed19224cdd1ba46611f4bd7bfe69b994
vinnow98 <evinwoon@gmail.com>
2022-03-29 13:35:39 +0800

    Finished activity 2

--- if.go
+++ if.go
@@ -15,4 +15,29 @@ func main() {
 	} else {
 		fmt.Printf(resultMessage)
 	}
+	fmt.Println("")
+	activity2()
+}
+func activity2() {
+	number1 := 17
+	resultMessage := "No outcome."
+	if number1%2 == 0 {
+		fmt.Print("This is an even number")
+	} else if number1%2 == 1 {
+		fmt.Println("This is an odd number")
+	} else {
+		fmt.Println(resultMessage)
+
+	}
+	if number1 > 9 {
+		fmt.Println("This number has more than 1 digit")
+	} else if number1 < -9 {
+		fmt.Println("This number has more than 1 digit")
+	} else {
+		fmt.Println("This number has only 1 digit")
+	}
+}
+
+func activity3() {
+
 }
